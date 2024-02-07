597e2418c7a788c88c0ab7420dfbfc8f7cd9304e
Sebb955 <slafrizi@gmail.com>
2024-02-07 15:11:54 +0100

    Ajoute les modifications pour l issue 9

---
+++ enemyTeam.py
@@ -0,0 +1,24 @@
+class Team:
+    def __init__(self):
+        pass
+
+    # Autres méthodes et attributs de la classe Team
+
+class EnemyTeam(Team):
+    def __init__(self, unit: str, damage: int, loot: int):
+        super().__init__()
+        self.__unit = unit  # Type d'unité (zombie, goblin, orc)
+        self.__damage = damage  # Dégâts totaux de l'équipe
+        self.__loot = loot  # Valeur de butin de l'équipe
+
+    # Méthode pour accéder au type d'unité
+    def get_unit_type(self) -> str:
+        return self.__unit
+
+    # Méthode pour accéder aux dégâts de l'équipe
+    def get_damage(self) -> int:
+        return self.__damage
+
+    # Méthode pour accéder à la valeur de butin de l'équipe
+    def get_loot(self) -> int:
+        return self.__loot
