e713e5502f03b8c0f7a9ad26c4a067b9fafc3bdc
Sebby75 <79416415+Sebb955@users.noreply.github.com>
2024-02-07 14:23:17 +0100

    Update orc.py
    Co-authored-by: Rayane <rayanelark@gmail.com>

--- orc.py
+++ orc.py
@@ -2,6 +2,6 @@ import random
 
 class Orc:
     def __init__(self):
-        self.degat = random.randint(3, 5)  # Génère un nombre aléatoire entre 3 et 5
+        self.degat = random.choice([3, 5])  # Génère un nombre aléatoire entre 3 et 5
         self.loot = random.uniform(2, 2.5)  # Génère un nombre à virgule flottante aléatoire entre 2 et 2.5
 
