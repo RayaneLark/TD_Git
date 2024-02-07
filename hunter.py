09c9b9faec9ca6009b6d8b5af3c5f571de199cf3
Rayane <rayanelark@gmail.com>
2024-02-07 14:11:35 +0100

    issue 1, 2, et 3
    Les trois classes ont été réalisées #1 #2 #3

---
+++ hunter.py
@@ -0,0 +1,9 @@
+import random
+
+class Hunter:
+    def __init__(self):
+        self.degat = random.choice([1, 2])
+        self.chance = 10
+        self.fuite = 20
+        self.prix = 25
+        self.type_unite = "hunter"
